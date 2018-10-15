707aa89efd5bbe61e53053b4c9c66e7e5c07fed1
anggikozeal <anggi.kozel@gmail.com>
2018-10-15 21:56:30 +0700

    produk terbaru

--- application/models/Mod_produk.php
+++ application/models/Mod_produk.php
@@ -17,7 +17,9 @@ class Mod_produk extends CI_Model {
                 t_produk.harga, 
                 t_produk.deskripsi,
                 t_produk.status,
-                t_produk.image,      
+                t_produk.image,  
+                t_kategori.id as id_kategori,
+                t_kategori.kategori,    
                 t_toko.id as id_toko,    
                 t_toko.nama_toko,
                 t_user.id as id_user,
@@ -25,6 +27,7 @@ class Mod_produk extends CI_Model {
                 ");
                 $this->db->from("t_produk");
                 $this->db->join("t_toko","t_produk.id_toko = t_toko.id","inner");
+                $this->db->join("t_kategori","t_produk.id_kategori = t_kategori.id","inner");
                 $this->db->join("t_user","t_user.id = t_toko.id_user","inner");
                 $this->db->order_by("t_produk.id","desc");
                 $this->db->limit($limit);
